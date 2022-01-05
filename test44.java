a225fe7c2762b7bccefaf2df9ad5ed85f635456b
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2022-01-05 22:30:52 +0900

    MTA끝

---
+++ test44.java
@@ -0,0 +1,26 @@
+
+public class test44 {
+
+	public static int main(String[] args) {
+		// TODO Auto-generated method stub
+		 int price = 0;
+		 int model = 5;
+		 switch (model) {
+		 case 'A':
+		        price = 50;
+		        break;
+		      case 'T':
+		        price = 20;
+		      case 'C':
+		        price = 5;
+		        break;
+		       default:
+		        price = 100;
+		        break;
+		    }
+		    return price;
+		
+
+	}
+
+}
