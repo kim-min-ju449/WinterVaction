5a13fb35c5829f5a6b892502cfbf9156b5589102
kim-min-ju449 <77184157+kim-min-ju449@users.noreply.github.com>
2022-01-07 21:07:24 +0900

    MTA 합격기념 커밋

---
+++ test43.java
@@ -0,0 +1,16 @@
+
+public class test43 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {1,2,3,4,5,6,7};
+		int sum=0;
+		for(int k:arr) {
+			sum+=k;
+		}
+		System.out.println(sum);
+
+	}
+
+}
